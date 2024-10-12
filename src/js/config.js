93990826edb531596d46ca06c33046c3a6b3f449
abodiamhe <akhigbesadiq@gmail.com>
2024-10-12 22:43:25 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3a3a04f4-87b4-409d-ab99-595215e0bc11';
+export const MODAL_CLOSE_SEC = 2.5;
